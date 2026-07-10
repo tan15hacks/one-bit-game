4e490bbb853079ba636930f69a4e71ba67fc7405
tan15hacks <110644530+tan15hacks@users.noreply.github.com>
2026-07-11 00:47:12 +0800

    test: update main menu expectations for tutorial flow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,10 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:one_bit_game/main.dart';
 
 void main() {
-  testWidgets('main menu renders the title and play button', (tester) async {
+  testWidgets('main menu renders the title and tutorial button', (tester) async {
     await tester.pumpWidget(const OneBitApp());
 
     expect(find.text('ONE BIT\nESCAPE'), findsOneWidget);
-    expect(find.text('PLAY'), findsOneWidget);
+    expect(find.text('START TUTORIAL'), findsOneWidget);
   });
 }
