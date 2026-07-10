d6365af9edd0fd1805df8a6d629f2357ca047191
tan15hacks <110644530+tan15hacks@users.noreply.github.com>
2026-07-10 23:24:03 +0800

    assets: add Kenney Tiled tileset definition

---
+++ assets/tiles/one_bit.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="kenney-one-bit" tilewidth="16" tileheight="16" tilecount="400" columns="20" spacing="1">
+ <image source="../images/monochrome_tilemap_transparent.png" width="339" height="339"/>
+</tileset>
